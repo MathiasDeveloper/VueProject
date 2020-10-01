b5b5671b03803b689f4c710b61e698d310799f95
MathiasRbr <mathias.robert35@hotmail.fr>
2020-10-01 15:59:43 +0200

    initial commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app-bar app color="green" class="d-flex justify-center">
+        <div>
+            <v-btn text exact :to="{name: 'Home'}" active-class="menu-active-link" > Accueil </v-btn> |
+            <v-btn text :to="{name: 'About'}" active-class="menu-active-link" > A propos </v-btn> |
+            <v-btn text :to="{name: 'Product'}" active-class="menu-active-link"> Produits </v-btn>
+        </div>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: "Menu"
+    }
+</script>
+
+<style lang="scss">
+.v-btn {
+    text-transform: none !important;
+    color: white!important;
+}
+.menu-active-link{
+    font-weight: bold!important;
+}
+.menu-active-link:before{
+    background-color: transparent!important;
+}
+</style>
---
+++ src/views/Product.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-container>
+        <h1>Product</h1>
+        <v-row>
+            <v-btn fab class="ma-2" color="primary" v-on:click="addProducts">
+                <v-icon>mdi-plus</v-icon>
+            </v-btn>
+            <p class="pt-6">Ajouter un produit</p>
+        </v-row>
+        <AddProduct v-if="add"></AddProduct>
+    </v-container>
+</template>
+
+<script>
+    import AddProduct from "../components/AddProduct";
+    export default {
+        name: "Product",
+        components: {
+            AddProduct
+        },
+        data () {
+            return {
+                add: false
+            }
+        },
+        methods: {
+            addProducts () {
+                this.add = this.add === false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
